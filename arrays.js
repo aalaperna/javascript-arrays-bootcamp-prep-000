2d0717d51cb07443cf2de0c314d987f1cda8b93e
Alyssa LaPerna <aalaperna@gmail.com>
2018-01-24 19:47:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray
-function destructivelyAddElementToBeginningOfArray (array, element);
+function destructivelyAddElementToBeginningOfArray (array, element); {
+}
 function addElementToEndOfArray
 function destructivelyAddElementToEndOfArray
