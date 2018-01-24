8c0ed80ce8648e424d978b2d656bb9a0011e58da
Alyssa LaPerna <aalaperna@gmail.com>
2018-01-24 20:16:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -18,6 +18,5 @@ function destructivelyAddElementToEndOfArray (array, element) {
   return array
 }
 function accessElementInArray (array, index) {
-  console.log(array[index])
-  return index
+  return (array[index])
   }
